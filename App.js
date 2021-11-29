569b6855852f6da997f8ad1dc90cb1ff2b083416
AlonsoLo-123 <71737976+AlonsoLo-123@users.noreply.github.com>
2021-11-29 02:49:47 -0600

    App xd

--- App.js
+++ App.js
@@ -1,21 +1,58 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, ImageBackground, Button, Alert, TouchableOpacity} from 'react-native';
+import raphtalia from './assets/raphtalia-pan.jpg';
 
-export default function App() {
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ImageBackground source = {{uri: 'https://k62.kn3.net/taringa/5/4/D/6/E/D/Drk-zero/1B1.jpg'}}  style = {PrimerStyle.ImgBack}>
+      <View style = {PrimerStyle.Center}>
+        <Text style = {PrimerStyle.Title}>Hola Mundo!</Text>
+        <Image source = {raphtalia}
+        style = {PrimerStyle.Size}
+        />
+        <Button
+          color = 'green'
+          title = ':d'
+          onPress = {() => Alert.alert('Presionado!')}
+        />
+        <TouchableOpacity 
+          onPress ={() => console.log('Boton pro xd')}
+          style = {PrimerStyle.ButtonOp}
+        >
+          <Text style = {{color: '#fff'}}>Boton pro</Text>
+        </TouchableOpacity>
+      </View>
+    </ImageBackground>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
+const PrimerStyle = StyleSheet.create({
+  Center: {
     alignItems: 'center',
     justifyContent: 'center',
   },
-});
+  ImgBack:{
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  Title:{
+    fontStyle:'italic',
+    fontSize:40,
+    color: '#000'
+  },
+  Size:{
+    height: 250,
+    width: 250,
+    borderRadius: 250
+  },
+  ButtonOp:{
+    backgroundColor: '#000',
+    marginTop: 3,
+    padding: 5,
+    fontSize: 15
+  }
+})
+
+export default App;
